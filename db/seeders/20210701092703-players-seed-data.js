eb91d5f5912285fbbe609272677d93bd491e477b
eddiejpot <jamesedwardporter@gmail.com>
2021-07-08 07:58:04 +0800

    modified seeders

--- db/seeders/20210701092703-players-seed-data.js
+++ db/seeders/20210701092703-players-seed-data.js
@@ -1,14 +1,14 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // Make playerList
-    const playerNames = ['sunny', 'abcde', 'kong', 'barbie', 'cassie', 'andrew', 'jon', 'kenny', 'aik', 'boi', 'JJ', 'Zack', 'John', 'Simbad'];
+    const playerNames = ['sunny', 'abcde', 'kong', 'barbie', 'cassie', 'andrew', 'jon', 'kenny', 'aik', 'boi', 'JJ', 'Zack', 'John', 'Simbad', 'Kenji', 'Ken', 'Philip', 'Draco', 'Voldermot', 'Ron', 'Weasley', 'Spyderman', 'Luke', 'Chicken', 'Banana', 'Rick', 'Morty', 'Bojack', 'DavyJones', 'Mummy', 'Maccas', 'ILoveToWin'];
     const playerList = [];
 
     for (let i = 0; i < playerNames.length; i += 1) {
       playerList.push(
         {
           name: playerNames[i],
-          score: Math.random() * 20 + 1, // random integer from 1 to 20:,
+          score: Math.random() * 50 + 1, // random integer from 1 to 50:,
           created_at: new Date(),
           updated_at: new Date(),
         },
