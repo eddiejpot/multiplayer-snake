ce21016d93b5be16204917ea8634062c0fc9cc3c
eddiejpot <jamesedwardporter@gmail.com>
2021-07-08 10:34:33 +0800

    added new script for dev env in package json

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,6 @@ import './styles/styles.css';
 // Tool tip for multiplayer
 
 const exampleEl = document.getElementById('multiplayer');
-exampleEl.setAttribute('title', 'The developer is still working on this feature 🐢');
+exampleEl.setAttribute('title', 'The tired developer is still working on this feature 🐢');
 exampleEl.setAttribute('data-bs-placement', 'bottom');
 const tooltip = new bootstrap.Tooltip(exampleEl);
